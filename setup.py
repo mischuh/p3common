b803417e4fd546333ff350cc54309b129eb07161
mirko.schuh <mirko.schuh@magicline.de>
2018-01-26 12:52:26 +0100

    setup tools to build package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+__VERSION__ = '0.1'
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='python3-common',
+      version=__VERSION__,
+      description='mischus - Python3 Common utility classes',
+      long_description=readme(),
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Python :: Common',
+      ],
+      url='https://github.com/mischuh/python3-common.git',
+      author='Mirko Schuh',
+      packages=find_packages(exclude=['docs', 'tests*']),
+      install_requires=[
+          "decorator>=4.2.1",
+      ],
+      entry_points={
+      },
+      include_package_data=True,
+      zip_safe=False)
